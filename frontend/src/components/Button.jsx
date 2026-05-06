a78b41346fa417c0402dafccc212b39ec6b7da62
ewenlarrieu <ewenlarrieu200515@gmail.com>
2026-05-06 14:55:10 +0200

    Hero section dessktop et mobile

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import '../styles/Button.css'
+
+function Button({ label }) {
+  return (
+    <button className="btn">{label}</button>
+  )
+}
+
+export default Button
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,22 @@
- export default function Home() {
+import '../styles/Home.css'
+import Button from '../components/Button'
+import HeroImage from '../assets/images/Heroimage.png'
+
+export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <section className="hero">
+        <div className="hero-container">
+          <div className="hero-content">
+            <div className="hero-text">
+              <h1 className="hero-title">Réparez votre vélo sans vous déplacer</h1>
+              <p className="hero-desc">Prenez rendez-vous avec un technicien à domicile, choisissez votre forfait d'entretien et suivez facilement vos interventions depuis votre espace client.</p>
+              <Button label="Commencer" />
+            </div>
+            <img src={HeroImage} alt="Technicien vélo" className="hero-image" />
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
